adf746af76d59253f6901662e3cfa2590acd5969
TaniaY21 <taniay@hotmail.co.uk>
2021-08-22 09:00:28 +0100

    modal created with basic styling

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,9 +1,28 @@
+import { useState } from 'react';
+import Modal from '../Layout/Modal';
+import Backdrop from '../Layout/Backdrop';
 const HomePage = () => {
-    return (
-        <div>
-            <h1>This is the homepage</h1>
-        </div>
-    )
-}
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
-export default HomePage
+  function startTaskHandler() {
+    setModalIsOpen(true);
+  }
+
+  function closeModalHandler() {
+    setModalIsOpen(false);
+  }
+
+  return (
+    <div>
+      <h1>This is the homepage</h1>
+      <button onClick={startTaskHandler}>START TASK: Modal test button</button>
+
+      {modalIsOpen && (
+        <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler} />
+      )}
+      {modalIsOpen && <Backdrop onCancel={closeModalHandler} />}
+    </div>
+  );
+};
+
+export default HomePage;
