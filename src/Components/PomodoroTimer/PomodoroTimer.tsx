69781fc77393dccef17de74855f3dff97405204b
Umar <umar97@hotmail.co.uk>
2021-08-22 06:25:44 +0100

    removed ugly comments

--- src/Components/PomodoroTimer/PomodoroTimer.tsx
+++ src/Components/PomodoroTimer/PomodoroTimer.tsx
@@ -101,35 +101,3 @@ const PomodoroTimer = () => {
   );
 };
 export default PomodoroTimer;
-
-//   const [time, setTime] = useState<number>(25 * 60 * 1000);
-
-//   const [pause, setPause] = useState(false);
-
-//   const onPause = () => {
-//     setPause(!pause);
-//   };
-
-//   const onStart = () => {
-//     if (pause === false) {
-//       setInterval(() => {
-//         setTime((time) => time - 1000);
-//       }, 1000);
-//     } else {
-//       setTime(time);
-//     }
-//   };
-
-//   const minutes = Math.floor(time / 60000);
-//   const seconds = ((time % 60000) / 1000).toFixed(0);
-
-//   return (
-//     <div className="App">
-//       <h1>Pomodoro Timer</h1>
-//       <h2>
-//         {minutes}:{seconds}
-//       </h2>
-//       <button onClick={onStart}>Start Timer</button>
-//       <button onClick={onPause}>Pause Timer</button>
-//     </div>
-//   );
