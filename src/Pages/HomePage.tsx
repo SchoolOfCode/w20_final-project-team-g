adee72d707b5cabd63e0f6a23582f68321dd8832
merlinjones10 <merlinjones10@gmail.com>
2021-08-24 00:48:14 +0100

    added user name on home page

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -3,13 +3,17 @@ import TodoList from "../Components/Todos/Todolist";
 import InProgress from "../Components/Todos/InProgress";
 import styles from "./HomePage.module.css";
 import CompletedTodos from "../Components/Todos/CompletedTodos";
-import { Fragment, useState } from "react";
+import { Fragment, useState, useContext } from "react";
+import { UserContext } from "../Store/UserContext";
 
 const HomePage = () => {
+  const {
+    userProfile: { name, email, uniqueID },
+  } = useContext(UserContext);
   return (
     <Fragment>
+      <h4>Welcome {name}</h4>
       <NewTodo />
-
       <div className={styles.flex}>
         <div className={styles.column}>
           <TodoList />
