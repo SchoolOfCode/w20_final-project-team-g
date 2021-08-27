d24b9aa22636c20d2eba438fa30d409820c11405
TaniaY21 <taniay@hotmail.co.uk>
2021-08-27 16:13:12 +0100

    conditional rendering added

--- src/Components/PomodoroTimer/PomodoroTimer.tsx
+++ src/Components/PomodoroTimer/PomodoroTimer.tsx
@@ -34,42 +34,50 @@ const PomodoroTimer = () => {
               </CountdownAnimation>
             </div>
           </div>
-          <div className={styles.buttonWrapper}>
-            <button
-              className={
-                !pomodoroCtx.startAnimate
-                  ? styles.active && styles.specialButton
-                  : undefined
-              }
-              onClick={pomodoroCtx.startTimer}
-            >
-              Start
-            </button>
-
-            <button
-              className={
-                pomodoroCtx.startAnimate
-                  ? styles.active && styles.specialButton
-                  : undefined
-              }
-              onClick={pomodoroCtx.pauseTimer}
-            >
-              Pause
-            </button>
-            <button onClick={pomodoroCtx.resetSettings}> Reset </button>
-          </div>
         </>
       ) : (
         <PomodoroSettings />
+      )}
+
+      {/* new extra conditional rendering*/}
+
+      {pomodoroCtx.pomodoro !== 0 && !pomodoroCtx.isTimerFinished && (
+        <div className={styles.buttonWrapper}>
+          <button
+            className={
+              !pomodoroCtx.startAnimate
+                ? styles.active && styles.specialButton
+                : undefined
+            }
+            onClick={pomodoroCtx.startTimer}
+          >
+            Start
+          </button>
+
+          <button
+            className={
+              pomodoroCtx.startAnimate
+                ? styles.active && styles.specialButton
+                : undefined
+            }
+            onClick={pomodoroCtx.pauseTimer}
+          >
+            Pause
+          </button>
+          <button onClick={pomodoroCtx.resetSettings}> Reset </button>
+        </div>
+      )}
+      {pomodoroCtx.isTimerFinished && !fetchModal && (
+        <button onClick={presentModal}>Click here to continue</button>
+      )}
+
+      {fetchModal && (
+        <Modal>
+          <YesNoModal />
+        </Modal>
       )}
     </div>
   );
 };
 
 export default PomodoroTimer;
-
-//   {pomodoroCtx.isTimerFinished && !fetchModal && (
-//     <button onClick={presentModal}>Click here to continue</button>
-//   )}
-//   {fetchModal && <Modal><YesNoModal/></Modal>}
-// </div>
