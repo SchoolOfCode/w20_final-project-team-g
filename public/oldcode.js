c655ecb377f33acf1a60393344cd014ed1675cd7
TaniaY21 <taniay@hotmail.co.uk>
2021-09-03 16:12:21 +0100

    removed some comments and reviewed merlin's code

--- public/oldcode.js
+++ public/oldcode.js
@@ -1,3 +1,43 @@
+// const fetchAndModifyData = () => {
+// fetch
+// moodRef.onSnapshot((querySnapshot) => {
+//   const newObj = [];
+//   querySnapshot.forEach((doc) => {
+//     newObj.push(doc.data());
+//   });
+//   // remove parent key
+//   const editedObj = removeJSONString(newObj);
+//   setAllMoods(editedObj);
+// });
+// extract all moods
+// console.log(allMoodsFromDB);
+// create the new mood object
+
+// console.log('NEW MOOD ARRAY FROM ASYNC: ' + moodArray);
+// console.log(moodArray);
+// console.log('moodArray inside fetchAndModifyData', moodArray);
+// console.log('allMoodsObj after method', allMoodsObj);
+// };
+// console.log('moodArray outside function', moodArray);
+
+// console.log('moodArray', moodArray);
+// function removeJSONString(obj) {
+//   // store all keys of this object for later use
+//   let keys = Object.keys(obj);
+//   // for each key update the "json" key
+//   keys.map((key) => {
+//     // updates only if it has "json"
+//     if (obj[key].hasOwnProperty('moodData')) {
+//       // assign the current obj a new field with "json" value pair
+//       Object.assign(obj[key], obj[key]['moodData']);
+//       // delete "json" key from this object
+//       delete obj[key]['moodData'];
+//     }
+//   });
+//   // updated all fields of obj
+//   return obj;
+// }
+
 // to delete from state & firebase
 // setTodos((prev) => {
 //   return prev.filter((todo) => {
@@ -10,45 +50,44 @@
 // const todoRef = firebase.database().ref('currentTodo').child(todosID);
 // todoRef.remove();
 
-    //mine
-    // todoref.on('value', (snapshot: any) => {
-    //   const todos = snapshot.val();
+//mine
+// todoref.on('value', (snapshot: any) => {
+//   const todos = snapshot.val();
 
-    //   const todolist = [];
-    //   for (const key in todos) {
-    //     todolist.push(todos[key]);
+//   const todolist = [];
+//   for (const key in todos) {
+//     todolist.push(todos[key]);
 
-    //     setTodos(todolist);
-    //   }
-    // });
+//     setTodos(todolist);
+//   }
+// });
 
-        //change status here to "done"
-    // setTodos((prevTodos) => {
-    //     return prevTodos.filter((todo) => todo.id === todoId) { todo.status === 'done' } });
-    // });
+//change status here to "done"
+// setTodos((prevTodos) => {
+//     return prevTodos.filter((todo) => todo.id === todoId) { todo.status === 'done' } });
+// });
 
+// Now working.The code below I don't believe is necessary as Firebase handles all the updates on their end.
+//Think of it like  every time we update/change/add/delete a to do its runs the setTodo hook for us. I think...
 
-    // Now working.The code below I don't believe is necessary as Firebase handles all the updates on their end.
-    //Think of it like  every time we update/change/add/delete a to do its runs the setTodo hook for us. I think...
+///////////////////////////////////// MIGHT BE NEEDED LATER IN PROJECT?///////
+// setTodos((prev) => {
+//   const indexToUpdate = prev.findIndex((todo) => todo.id === selectedTodo.id);
+//   const oldValue = prev[indexToUpdate];
+//   const newValue = Object.assign({}, oldValue, {
+//     status: TodoStatus.inProgress,
+//   });
+//   const updatedTodos = [
+//     // all items to left of what we are updating
+//     ...prev.slice(0, indexToUpdate),
+//     // the updated value
+//     newValue,
+//     // all the items to the right of what we are updating
+//     ...prev.slice(indexToUpdate + 1),
+//     // skip what was updated
+//   ];
 
-    ///////////////////////////////////// MIGHT BE NEEDED LATER IN PROJECT?///////
-    // setTodos((prev) => {
-    //   const indexToUpdate = prev.findIndex((todo) => todo.id === selectedTodo.id);
-    //   const oldValue = prev[indexToUpdate];
-    //   const newValue = Object.assign({}, oldValue, {
-    //     status: TodoStatus.inProgress,
-    //   });
-    //   const updatedTodos = [
-    //     // all items to left of what we are updating
-    //     ...prev.slice(0, indexToUpdate),
-    //     // the updated value
-    //     newValue,
-    //     // all the items to the right of what we are updating
-    //     ...prev.slice(indexToUpdate + 1),
-    //     // skip what was updated
-    //   ];
-
-    //   return updatedTodos;
-    // });
-    //////////////////////////// MIGHT BE NEEDED LATER IN PROJECT?///////
-    //
+//   return updatedTodos;
+// });
+//////////////////////////// MIGHT BE NEEDED LATER IN PROJECT?///////
+//
--- src/Components/Moodtracker/DoughnutChart.tsx
+++ src/Components/Moodtracker/DoughnutChart.tsx
@@ -1,87 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import { Doughnut } from 'react-chartjs-2'; // chart js
 import firebase from 'firebase';
-// import 'rxjs/add/operator/map';
 
 const DoughnutChart = () => {
   const moodRef = firebase.firestore().collection('moods');
-  // const [allMoods, setAllMoods] = useState([]);
-  // const [moodArrayNumbers, setMoodArrayNumbers] = useState([]);
-  // const [moodOccurencesObj, setMoodOccurencesObj] = useState([]);
   const [moodArrayData, setMoodArrayData] = useState(null);
-  // const allMoodsObj = { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 };
 
   useEffect(() => {
     const getMoodData = async () => {
-      const countOccurrences = (arr, val) => arr.reduce((a, v) => (v === val ? a + 1 : a), 0); // funciton to count each occurence of a number in database
-      //.map does not work in objects in TS? so pushing all items to an empty array first (unless we specify type in TS??)
+      // funciton to count each occurence of a number in databases
+      const countOccurrences = (arr, val) =>
+        arr.reduce((a, v) => (v === val ? a + 1 : a), 0);
+
       let fetchedMoodsNumbersOnly = [];
-      let fetchedMoodsAllData = [];
       const data: any = await moodRef.get();
       data.forEach((doc) => {
         //Only mapping the 'mood' NUMBER to fetchedMoods.
-        //I tried converting the data to numbers to fix the pie chart issue but it didnt work so Number() could be removed?
         fetchedMoodsNumbersOnly.push(Number(doc.data().moodData.mood));
-        //All data in an array if we want to use other data in future
-        fetchedMoodsAllData.push(doc.data().moodData);
+        console.log('fetchedMoodsNumbersOnly', fetchedMoodsNumbersOnly);
       });
-      // const sortedMoods = fetchedMoodsNumbersOnly.sort((a, b) => a - b); // not using this state
-      // setMoodArrayNumbers(sortedMoods); not usin this state
+      // get number of occurances for each mood (1-5)
       const num1Ocs = countOccurrences(fetchedMoodsNumbersOnly, 1);
+      console.log(num1Ocs);
       const num2Ocs = countOccurrences(fetchedMoodsNumbersOnly, 2);
       const num3Ocs = countOccurrences(fetchedMoodsNumbersOnly, 3);
       const num4Ocs = countOccurrences(fetchedMoodsNumbersOnly, 4);
       const num5Ocs = countOccurrences(fetchedMoodsNumbersOnly, 5);
+      console.log(num5Ocs);
+
       setMoodArrayData([num1Ocs, num2Ocs, num3Ocs, num4Ocs, num5Ocs]);
-      console.log(moodArrayData);
     };
 
     getMoodData();
-    // console.log(moodArray);
-    // const allMoodsFromDB = mjMoodArray.map((item) => item.moodData.mood).sort((a, b) => a - b);
-
-    // console.log(mjMoodArray[0].moodData.mood); // <== just get mood example
   }, []);
 
-  // const fetchAndModifyData = () => {
-  // fetch
-  // moodRef.onSnapshot((querySnapshot) => {
-  //   const newObj = [];
-  //   querySnapshot.forEach((doc) => {
-  //     newObj.push(doc.data());
-  //   });
-  //   // remove parent key
-  //   const editedObj = removeJSONString(newObj);
-  //   setAllMoods(editedObj);
-  // });
-  // extract all moods
-  // console.log(allMoodsFromDB);
-  // create the new mood object
-
-  // console.log('NEW MOOD ARRAY FROM ASYNC: ' + moodArray);
-  // console.log(moodArray);
-  // console.log('moodArray inside fetchAndModifyData', moodArray);
-  // console.log('allMoodsObj after method', allMoodsObj);
-  // };
-  // console.log('moodArray outside function', moodArray);
-
-  // console.log('moodArray', moodArray);
-  // function removeJSONString(obj) {
-  //   // store all keys of this object for later use
-  //   let keys = Object.keys(obj);
-  //   // for each key update the "json" key
-  //   keys.map((key) => {
-  //     // updates only if it has "json"
-  //     if (obj[key].hasOwnProperty('moodData')) {
-  //       // assign the current obj a new field with "json" value pair
-  //       Object.assign(obj[key], obj[key]['moodData']);
-  //       // delete "json" key from this object
-  //       delete obj[key]['moodData'];
-  //     }
-  //   });
-  //   // updated all fields of obj
-  //   return obj;
-  // }
+  console.log('moodArrayData', moodArrayData);
 
   const data = {
     labels: ['Very Sad', 'Sad', 'Ok', 'Happy', 'Very Happy'], // hover effect
