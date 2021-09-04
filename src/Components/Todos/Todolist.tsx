bbcda3f24fd02cf4fa3e144c3acdaa67d983ebb0
merlinjones10 <merlinjones10@gmail.com>
2021-09-04 01:07:32 +0100

    sorting drop down works. Need to test createdBY

--- src/Components/Todos/Todolist.tsx
+++ src/Components/Todos/Todolist.tsx
@@ -6,29 +6,46 @@ import Modal from '../../Layout/Modal';
 import Backdrop from '../../Layout/Backdrop';
 import PomodoroTimer from '../PomodoroTimer/PomodoroTimer';
 import styles from './Todolist.module.css';
-import { Select } from '@material-ui/core';
+import { MenuItem, Select } from '@material-ui/core';
+import { InputLabel } from '@material-ui/core';
 
 const TodoList: React.FC = () => {
   const todoCtx = useContext(TodosContext);
-  const [sorted, setSorted] = useState(null);
+  const [sortBy, setSortBy] = useState('Urgency');
 
   function closeModalHandler() {
     todoCtx.closeModal();
   }
-
+  const handleChange = (e) => {
+    console.log(e.target.value);
+    setSortBy(e.target.value);
+    if (e.target.value === 'urgency') {
+      todoCtx.items.sort((a, b) => (b.urgency > a.urgency ? -1 : 1));
+    }
+    if (e.target.value === 'createdBy') {
+      todoCtx.items.sort((a, b) => (b.createdBy > a.createdBy ? -1 : 1));
+    }
+  };
   useEffect(() => {
     todoCtx.retrieveCurrentTodo();
+    // set;
   }, []);
   // todoCtx.items.sort((a, b) => (b.urgency > a.urgency ? -1 : 1));
   return (
     <Fragment>
-      <button
+      <InputLabel id="sort-by-label">Sort By</InputLabel>
+
+      <Select labelId="select-label" id="select-label" value={sortBy} onChange={handleChange}>
+        <MenuItem value={'urgency'}>Urgency</MenuItem>
+        <MenuItem value={'createdBy'}>Created By</MenuItem>
+      </Select>
+      {/* <button
         onClick={() => {
           setSorted(todoCtx.items.sort((a, b) => (b.urgency > a.urgency ? -1 : 1)));
         }}
       >
         Sort
-      </button>
+      </button> */}
       <h3 className="text-gray-500 tracking-wide font-medium text-2xl mt-4 mb-4">Backlog</h3>
       <ul className="w-72">
         {todoCtx.items &&
