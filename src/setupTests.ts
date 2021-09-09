09440e45ad4604a5d62fba4dc3ef08a8fa637f07
TaniaY21 <taniay@hotmail.co.uk>
2021-09-09 16:49:06 +0100

    just some testing ideas

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,14 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// 3 'As' arrange, act, assert the result
+
+// testing ideas:
+// if todo is clicked timer is displayed
+// if todo is clicked its moved to inprogress
+// if tasked is marked as finished in modal it moves to "done"
+// averages for moodchart?
+
+// testing suites 
+// describe("description of suite", your tests)
