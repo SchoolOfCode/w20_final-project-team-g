1fdea6145c65a6aa5facc7c19094c0d5442a1f1f
TaniaY21 <taniay@hotmail.co.uk>
2021-09-10 07:25:53 +0100

    more pseudo code

--- src/Components/Todos/TodoItem.test.tsx
+++ src/Components/Todos/TodoItem.test.tsx
@@ -1,26 +1,25 @@
 import '@testing-library/jest-dom';
 import PomodoroTimer from '../PomodoroTimer/PomodoroTimer';
 import TodoItem from './TodoItem';
-import userEvent from '@testing-library/user-event'
+import userEvent from '@testing-library/user-event';
 
 describe('Todoitem component', () => {
+
+   // // is this an integration test?! 
   // test('renders the pomodoro timer component when it is clicked', () => {
 
-  //   // arrange 
+  //   // arrange
   //   render(<TodoItem text="hi" createdBy="string" todoBody="string" onRemoveTodo={() => void} />);
 
-  //   // act 
+  //   // act
   //   const startTodoDiv = screen.getByTestId("startTodoDiv")
   // userEvent.click(startTodoDiv)  // element that simulates click
 
-  // //assert the outcome 
+  // //assert the outcome
   //   const pomoClock = screen.get(<PomodoroTimer/>) // lol
   //   expect(pomoClock).toBeInTheDocument()
 
-
   // });
 
-  
-  test('renders blah blah ', () => {});
+  test('does not render pomodoro timer if delete button is clicked', () => {});
 });
-
